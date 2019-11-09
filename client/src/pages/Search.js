367f307cd0d810cf2303070b611ced5864966a53
Peter Geyser <peterpgeyser@gmail.com>
2019-11-08 19:23:55 -0800

    Search bar + handleInputChange functional

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container } from 'reactstrap';
+import { Card, CardHeader, CardBody, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import AppJumbotron from "./../components/Jumbotron";
 
 class Search extends Component {
@@ -7,10 +7,37 @@ class Search extends Component {
         searchbar: "",
     };
 
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+      };
+
     render() {
         return (
         <div>
             <AppJumbotron />
+
+            <Card>
+                <CardHeader>Book Search</CardHeader>
+                <CardBody>
+
+                    <Form>
+                        <FormGroup>
+                            <Label>Book</Label>
+                            <Input 
+                            value={this.state.searchbar}
+                            onChange={this.handleInputChange}
+                            name="searchbar"
+                            placeholder="Enter book title here" />
+                        </FormGroup>
+                        <Button>Search</Button>
+                    </Form>
+
+                </CardBody>
+        </Card>
+
         </div>
         )
     }
