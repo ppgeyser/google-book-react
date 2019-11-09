7c740fe8a6acdf7ce071cd4f821a2a54d1ca32b2
Peter Geyser <peterpgeyser@gmail.com>
2019-11-08 17:09:17 -0800

    Moved jumbotron to saved and search pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
-// import Jumbotron from "./components/Jumbotron";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
@@ -11,7 +10,6 @@ function App() {
     <Router>
       <div>
         <Nav />
-        {/* <Jumbotron /> */}
         <Switch>
           <Route exact path="/" component={Search} />
           <Route exact path="/search" component={Search} />
