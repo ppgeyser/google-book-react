ae26f0f4828797965d0fa966e9258ca95ce54ee4
Peter Geyser <peterpgeyser@gmail.com>
2019-11-08 23:22:45 -0800

    Made content change to jumbotron headline

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -6,7 +6,7 @@ const AppJumbotron = (props) => {
     <div>
       <Jumbotron fluid>
         <Container>
-          <h1 className="display-3 text-center">(React) Google Books Search</h1>
+          <h1 className="display-3 text-center">Google Books (React) Search</h1>
           <p className="lead text-center">Search for and save books of interest</p>
         </Container>
       </Jumbotron>
